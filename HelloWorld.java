bbbf6a00c333a1a8a6fed6d70dfca0db2b3e17f3
007anthony <anthypena@gmail.com>
2023-03-23 09:02:33 +0100

    feature A - new constructor

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
 public class HelloWorld {
 	
 	private int i;
-	public HelloWorld {
+	private String text;
+	public HelloWorld() {
 		i = 0;
 	
+	}
+
+	public HelloWorld(String text) {
+		this.text = text;
 	}
 
 	public int getI() {
