76d273731fbd29c9ce8a7dac2de30c2eaa10fbfb
007anthony <anthypena@gmail.com>
2023-03-28 21:47:36 +0200

    merge branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
 public class HelloWorld {
 	
 	private int i;
-<<<<<<< HEAD
 	private String name;
-=======
 	private String text;
->>>>>>> ddc8473da92ddbe655ed913bcb7c918513d8bf7a
 	public HelloWorld() {
 		i = 0;
 		this.text = "";
